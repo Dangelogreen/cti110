7a80b63cd8046c5ef4eb30a39db8e06fb75b2ba3
Dangelogreen <greend2970@student.faytechcc.edu>
2026-06-20 15:25:30 -0500

    Add files via upload

---
+++ p1lab1GreenDangelo.py
@@ -0,0 +1,9 @@
+# Green D'Angelo
+# 06/20/2026
+# p1lab1GreenDangelo.py
+# input and print
+
+first_name = input("Enter your first name: ")
+last_name = input("Enter your last name: ")
+
+print("Hello,", first_name,last_name  + "!", "Welcome to CTI-110")
